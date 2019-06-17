75d10dd217696267de8649b819861522cab5dc99
Basil <mbonubasil@gmail.com>
2019-06-17 07:00:04 +0100

    Get the server up and running

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const express =  require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express(),
-	 	  
-//Requiring routes
-const router = require('./app/routes/route');
-const User = require('./app/models/User');
-
-const mongodb = require('./config/mongoDB');
-
+const app = express();
+const mongodb = require("./config/mongoDB");
 
 const PORT = process.env.PORT || 3000;
 
 app.use((err, req, res, next) => {
-	res.status(404).json({ msg: 'Page not found'});
-})
+  res.status(404).json({ msg: "Page not found" });
+});
 
 // app.use(router);
 
-app.listen(PORT, () =>{
-	console.log(`app started on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`app started on port ${PORT}`);
 });
